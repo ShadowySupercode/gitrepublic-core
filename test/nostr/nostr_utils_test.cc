07085fbae1bd34f6d1e88a0a68359690d24910dc
Michael Jurkoic <mjjurkoic@gmail.com>
2024-02-18 10:53:56 -0600

    Add unit tests for NostrUtils constructor

--- test/nostr/nostr_utils_test.cc
+++ test/nostr/nostr_utils_test.cc
@@ -6,6 +6,21 @@
 
 using namespace std;
 
+TEST(NostrUtilsTest, ConstructorSetsEmptyDefaultRelays)
+{
+    nostr::NostrUtils* nostrUtils = new nostr::NostrUtils(new plog::ConsoleAppender<plog::TxtFormatter>());
+    ASSERT_EQ(nostrUtils->defaultRelays().size(), 0);
+    delete nostrUtils;
+};
+
+TEST(NostrUtilsTest, ConstructorSetsDefaultRelays)
+{
+    nostr::RelayList relays = { "wss://relay.damus.io", "wss://nostr.thesamecat.io" };
+    nostr::NostrUtils* nostrUtils = new nostr::NostrUtils(new plog::ConsoleAppender<plog::TxtFormatter>(), relays);
+    ASSERT_EQ(nostrUtils->defaultRelays().size(), 2);
+    delete nostrUtils;
+};
+
 class NostrUtilsTest : public testing::Test
 {
 protected:
